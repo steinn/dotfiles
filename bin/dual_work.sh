941b89b21002854cf721c9f754dbcf252e0fe868
Steinn Steinsen <steinnsteinsen@gmail.com>
2013-11-06 16:23:47 -0800

    bin: update monitor setup scripts

--- bin/dual_work.sh
+++ bin/dual_work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 240x1200 --rotate normal --output VGA1 --mode 1920x1200 --pos 0x0 --rotate normal
+xrandr --output DP3 --off --output DP2 --off --output DP1 --mode 1920x1200 --pos 1366x0 --rotate normal --primary --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x432 --rotate normal --output VGA1 --off
