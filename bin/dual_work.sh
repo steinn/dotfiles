3972b18dff09efe508001ef58b06663fc112b4ca
Steinn Steinsen <steinnsteinsen@gmail.com>
2015-01-20 09:39:01 +0000

    bin: display scripts

--- bin/dual_work.sh
+++ bin/dual_work.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
-xrandr \
-    --output DP3 --off \
-    --output DP2 --off \
-    --output DP1 --mode 1920x1200 --pos 1366x0 --rotate normal --primary \
-    --output HDMI3 --off \
-    --output HDMI2 --off \
-    --output HDMI1 --off \
-    --output LVDS1 --mode 1366x768 --pos 0x432 --rotate normal \
-    --output VGA1 --off
+
+function dual_work ()
+{
+    xrandr --output eDP1 --mode 2560x1600 \
+           --output DP1 --mode 2560x1440 --scale 1.5x1.5 --right-of eDP1 --primary \
+           --output HDMI2 --off \
+           --output HDMI1 --off \
+           --output DP2 --off \
+           --output VIRTUAL1 --off
+}
+
+dual_work
+
+# 'fixes' mouse
+xrandr --output DP1 --panning 3840x2160
+
+# normal
+xrandr --output VIRTUAL1 --off \
+       --output eDP1 --mode 2560x1600 --pos 0x0 --rotate normal \
+       --output DP1 --off \
+       --output HDMI2 --off \
+       --output HDMI1 --off \
+       --output DP2 --off
+
+
+# And finally now this works
+dual_work
--- bin/normal_work.sh
+++ bin/normal_work.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-xrandr \
-    --output DP3 --off \
-    --output DP2 --off \
-    --output DP1 --off \
-    --output HDMI3 --off \
-    --output HDMI2 --off \
-    --output HDMI1 --off \
-    --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --primary \
-    --output VGA1 --off
+xrandr --output VIRTUAL1 --off \
+       --output eDP1 --mode 2560x1600 --pos 0x0 --rotate normal \
+       --output DP1 --off \
+       --output HDMI2 --off \
+       --output HDMI1 --off \
+       --output DP2 --off
