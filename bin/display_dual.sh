b391edcd503a461b18014f6d47bbd7f6e4241d12
Steinn Steinsen <steinnsteinsen@gmail.com>
2016-08-01 22:44:44 +0000

    dual script for new work monitor

--- bin/display_dual.sh
+++ bin/display_dual.sh
@@ -1,22 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-xrandr --output eDP1 --off \
-       --output HDMI2 --preferred \
+# xrandr --output eDP1 --off \
+#        --output HDMI2 --preferred
 
-xrandr --output eDP1 --preferred \
-       --output HDMI2 --off \
+# xrandr --output eDP1 --preferred \
+#        --output HDMI2 --off
 
-xrandr --output HDMI2 --mode 2560x1440 --right-of eDP1 --scale 1.5x1.5 --primary \
+xrandr --output HDMI2 --mode 2560x1440 --right-of eDP1 --scale 1.25x1.25 --primary \
+       --output eDPI1 --preferred
 
-# 'fixes' mouse
 xrandr --output HDMI2 --panning 3840x2160/3840x2160x0x0
 
-
 xrandr --output eDP1 --off \
-       --output HDMI2 --preferred \
+       --output HDMI2 --off
 
 xrandr --output eDP1 --preferred \
-       --output HDMI2 --off \
+       --output HDMI2 --off
 
-
-xrandr --output HDMI2 --preferred  --right-of eDP1 --scale 1.5x1.5 --primary
+xrandr --output HDMI2 --mode 2560x1440 --right-of eDP1 --scale 1.25x1.25 --primary \
+       --output eDPI1 --preferred
