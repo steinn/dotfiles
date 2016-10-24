046fb6bad9c21519c29bd0b92a31eda19e4938cb
Steinn Steinsen <steinnsteinsen@gmail.com>
2016-10-24 09:34:48 +0000

    update display scripts

--- bin/display_dual.sh
+++ bin/display_dual.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-xrandr --output DP-2-1 --off --output DP-2-2 --primary --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-2-3 --off --output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
+xrandr --output DP-2-1 --off \
+       --output DP-2-2 \
+         --primary \
+         --mode 1920x1080 \
+         --pos 2560x360 \
+         --rotate normal \
+         --scale 1.5x1.5 \
+       --output DP-2-3 --off \
+       --output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal \
+       --output HDMI-2 --off \
+       --output HDMI-1 --off \
+       --output DP-2 --off \
+       --output DP-1 --off
