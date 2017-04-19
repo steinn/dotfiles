6edd9c92cd81a1ca973a16f80abd5da110fb4895
Steinn Steinsen <steinnsteinsen@gmail.com>
2017-04-19 09:15:39 +0000

    display_dual

--- bin/display_dual.sh
+++ bin/display_dual.sh
@@ -5,10 +5,20 @@ xrandr --output DP-2-1 --off \
          --mode 1920x1080 \
          --pos 2560x360 \
          --rotate normal \
-         --scale 1.5x1.5 \
        --output DP-2-3 --off \
-       --output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal \
+       --output eDP-1 \
+         --mode 2560x1440 \
+         --pos 0x0 \
+         --rotate normal \
        --output HDMI-2 --off \
        --output HDMI-1 --off \
        --output DP-2 --off \
        --output DP-1 --off
+
+sleep 5
+
+xrandr --output DP-2-2 --scale 1.5x1.5
+
+sleep 5
+
+xrandr --output DP-2-2 --panning 2880x1620+2560+360
